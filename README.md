8592204110a4223127c5ff0e43fb680a09083e48
saintsauce <t.donghao.zeng@gmail.com>
2024-06-08 16:40:05 -0400

    noice

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PacGPT
+
+If you are rich, and that you can afford OpenAI credits, feel free to take on an amazing adventure!
--- demo.py
+++ demo.py
@@ -1,53 +1,22 @@
-import configparser
-from openai import OpenAI # type: ignore
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
-# Read the API key from the config.ini file
-config = configparser.ConfigParser()
-config.read('config.ini')
-api_key = config['OpenAI']['API_KEY']
+# Load environment variables from .env file
+load_dotenv()
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key = api_key,
+# Access environment variables
+API_KEY = os.getenv('API_KEY')
+
+# Initialize OpenAI Client
+client = OpenAI(api_key=API_KEY)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ],
 )
 
-client.models.list()
-
-assistant = client.beta.assistants.create(
-    name = "Terminal Assistant",
-    instructions = "Assist me in downloading packages for arch linux.",
-    tools = [{"type": "code_interpreter"}],
-    model = "gpt-4o"
-)
-
-thread = client.beta.threads.create()
-print(thread)
-
-# Prompt the user for input
-user_input = input("Enter your query: ")
-
-message = client.beta.threads.messages.create(
-    thread_id = thread.id,
-    role = "user",
-    content = "Find out what package I need to download from my goal: {user_input}"
-)
-
-print(message)
-
-run = client.beta.threads.runs.create(
-    thread_id = thread.id,
-    assistant_id = assistant.id
-)
-
-run = client.beta.threads.runs.retrieve(
-    thread_id = thread.id,
-    run_id = run.id
-)
-
-messages = client.beta.threads.messages.list(
-    thread_id = thread.id
-)
-
-# get oldest messages first
-for message in reversed(messages.data):
-    print(message.role + ": " + message.content[0].text.value)
+print(completion.choices[0].message.value)
